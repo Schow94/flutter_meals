67de96633e46732492ee73b3317837eb8e3a4a63
Stephen Chow <stephenchow@Stephens-MacBook-Pro-2.local>
2020-09-02 08:27:23 -0700

    Style GridView

---
+++ lib/dummy_data.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick and Easy',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c3',
+    title: 'French',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Japansese',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Cuban',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Norwegian',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Chinese',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Mexican',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Italian',
+    color: Colors.black,
+  ),
+  Category(
+    id: 'c1',
+    title: 'Quick and Easy',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c1',
+    title: 'French',
+    color: Colors.lime,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Japansese',
+    color: Colors.grey,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Cuban',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c11',
+    title: 'Norwegian',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c12',
+    title: 'Chinese',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c13',
+    title: 'Mexican',
+    color: Colors.indigo,
+  ),
+];
